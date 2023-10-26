ae64617a02dec445845babbed0296c46dadc3682
arthur-siqueira <arthuths13@gmail.com>
2023-10-25 21:31:07 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/persnonapi/PersnonapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.persnonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersnonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersnonapiApplication.class, args);
+	}
+
+}
